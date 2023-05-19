1748f80a4cd23945ab6437531cdbfea05f238b7e
Saddam Azy <myxzlpltk@gmail.com>
2023-05-20 06:37:14 +0700

    datastore module

--- app/src/main/java/com/bangkit/coffee/di/RepositoryModule.kt
+++ app/src/main/java/com/bangkit/coffee/di/RepositoryModule.kt
@@ -1,9 +1,5 @@
 package com.bangkit.coffee.di
 
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import com.bangkit.coffee.data.repository.ImageDetectionRepository
 import com.bangkit.coffee.data.repository.ImageDetectionRepositoryImpl
 import com.bangkit.coffee.data.repository.UserPreferencesRepository
@@ -13,8 +9,6 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "datastore")
 
 @Module
 @InstallIn(SingletonComponent::class)
