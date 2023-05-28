8b1f1b47a2d850359910c6bb2fa45156a252dfa5
Saddam Azy <myxzlpltk@gmail.com>
2023-05-28 10:00:05 +0700

    disease detail test

---
+++ app/src/androidTest/java/com/bangkit/coffee/presentation/diseasedetail/DiseaseDetailRouteTest.kt
@@ -0,0 +1,42 @@
+package com.bangkit.coffee.presentation.diseasedetail
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithText
+import androidx.lifecycle.SavedStateHandle
+import com.bangkit.coffee.R
+import com.bangkit.coffee.presentation.ComposeTest
+import com.bangkit.coffee.util.AppTest
+import org.junit.Before
+import org.junit.Test
+
+class DiseaseDetailRouteTest : ComposeTest() {
+
+    private lateinit var viewModel: DiseaseDetailViewModel
+
+    @Before
+    fun setUp() {
+        val savedStateHandle = SavedStateHandle(mapOf("id" to "default-id"))
+        viewModel = DiseaseDetailViewModel(savedStateHandle = savedStateHandle)
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun should_showDiseaseInfo_when_loaded() {
+        rule.setContent {
+            AppTest {
+                DiseaseDetailRoute(
+                    coordinator = rememberDiseaseDetailCoordinator(viewModel)
+                )
+            }
+        }
+
+        rule.waitUntilExactlyOneExists(hasText(R.string.how_to_control), 10_000)
+        rule.waitUntil(10_000) { viewModel.stateFlow.value is DiseaseDetailState.Success }
+
+        val disease = (viewModel.stateFlow.value as DiseaseDetailState.Success).disease
+
+        rule.onNodeWithText(disease.name).assertExists()
+        rule.onNodeWithText(disease.description).assertExists()
+        disease.controls.forEach { rule.onNodeWithText(it).assertExists() }
+    }
+}
