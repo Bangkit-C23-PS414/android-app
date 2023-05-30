7b1b2b059c352c192d8ceab051439909f0e602f1
Saddam Azy <myxzlpltk@gmail.com>
2023-05-30 10:14:14 +0700

    init project

--- app/src/main/java/com/bangkit/coffee/shared/wrapper/Resource.kt
+++ app/src/main/java/com/bangkit/coffee/shared/wrapper/Resource.kt
@@ -1,7 +1,10 @@
 package com.bangkit.coffee.shared.wrapper
 
-sealed class Resource<out T> {
-    object Loading : Resource<Nothing>()
-    data class Error(val message: String) : Resource<Nothing>()
-    data class Success<out T>(val data: T) : Resource<T>()
+sealed class Resource<T>(
+    val data: T? = null,
+    val error: Throwable? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Error<T>(throwable: Throwable, data: T? = null) : Resource<T>(data, throwable)
 }
