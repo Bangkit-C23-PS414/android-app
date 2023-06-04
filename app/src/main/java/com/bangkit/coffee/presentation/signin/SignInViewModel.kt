c86de676a2d776d3e3961977d0187d3517652733
hollowsyde <alfatih.aditya@ui.ac.id>
2023-06-04 15:12:36 +0700

    Implement sign in logics and connect with api

--- app/src/main/java/com/bangkit/coffee/presentation/signin/SignInViewModel.kt
+++ app/src/main/java/com/bangkit/coffee/presentation/signin/SignInViewModel.kt
@@ -1,22 +1,28 @@
 package com.bangkit.coffee.presentation.signin
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bangkit.coffee.data.repository.UserPreferencesRepository
+import com.bangkit.coffee.data.source.remote.AuthService
+import com.bangkit.coffee.data.source.remote.RemoteUtil
+import com.bangkit.coffee.data.source.remote.model.LoginUser
 import com.bangkit.coffee.presentation.signin.components.SignInForm
 import com.bangkit.coffee.shared.wrapper.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import javax.inject.Inject
-import kotlin.random.Random
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val authService: AuthService,
+    private val userPreferencesRepository: UserPreferencesRepository
 ) : ViewModel() {
 
     private val _stateFlow = MutableStateFlow(SignInState())
@@ -31,21 +37,38 @@ class SignInViewModel @Inject constructor(
     fun signIn(formData: SignInForm) {
         viewModelScope.launch {
             _stateFlow.update { it.copy(inProgress = true) }
-            delay(1000)
-            if (Random.nextBoolean()) {
+
+            val (email, password) = formData
+            val loginUser = LoginUser(email, password)
+
+            try {
+                val response = authService.login(loginUser)
                 _stateFlow.update {
                     it.copy(
                         inProgress = false,
-                        message = Event("Something went wrong")
+                        message = Event("Signed in")
                     )
                 }
-            } else {
+                userPreferencesRepository.updateToken(response.token)
                 _stateFlow.update {
                     it.copy(
-                        inProgress = false,
                         signedIn = true
                     )
                 }
+                Log.d("SignInViewModel", "logged in with token ${response.token}")
+            } catch (e: HttpException) {
+                if (e.code() == 401) {
+                    val errorBody = e.response()?.errorBody()?.string()
+                    val errorMessage = RemoteUtil.extractErrorMessageFromJson(errorBody)
+                    _stateFlow.update {
+                        it.copy(
+                            inProgress = false,
+                            message = Event(errorMessage)
+                        )
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d("SignInViewModel", e.toString())
             }
         }
     }
