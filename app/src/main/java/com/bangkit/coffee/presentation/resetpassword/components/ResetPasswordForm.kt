d1df7887933dd31661d6cf45f0deb8610c12e497
Saddam Azy <myxzlpltk@gmail.com>
2023-06-10 10:35:10 +0700

    password min digit

--- app/src/main/java/com/bangkit/coffee/presentation/resetpassword/components/ResetPasswordForm.kt
+++ app/src/main/java/com/bangkit/coffee/presentation/resetpassword/components/ResetPasswordForm.kt
@@ -3,7 +3,7 @@ package com.bangkit.coffee.presentation.resetpassword.components
 import me.naingaungluu.formconductor.annotations.MinLength
 
 data class ResetPasswordForm(
-    @MinLength(6)
+    @MinLength(8)
     val password: String = "",
 
     @ResetPasswordConfirmPassword
