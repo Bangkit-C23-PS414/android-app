5ca0761b177e674b779e27c46284c68a7a03afbb
Alfatih Aditya <73227141+hollowsyde@users.noreply.github.com>
2023-06-11 09:33:57 +0700

    Fix OTPForm MinLength

--- app/src/main/java/com/bangkit/coffee/presentation/verifyotp/components/VerifyOTPForm.kt
+++ app/src/main/java/com/bangkit/coffee/presentation/verifyotp/components/VerifyOTPForm.kt
@@ -4,7 +4,7 @@ import me.naingaungluu.formconductor.annotations.MaxLength
 import me.naingaungluu.formconductor.annotations.MinLength
 
 data class VerifyOTPForm(
-    @MinLength(8)
+    @MinLength(6)
     @MaxLength(6)
     val code: String = "",
 )
